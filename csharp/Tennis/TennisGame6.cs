6323012dbcc77f46305ad9ed6daf2d601f786b53
Gustav Arneving <gustav.arneving@mindroad.se>
2024-03-03 22:16:59 +0100

    6: Move switches to methods

--- csharp/Tennis/TennisGame6.cs
+++ csharp/Tennis/TennisGame6.cs
@@ -1,3 +1,5 @@
+using System.Diagnostics;
+
 namespace Tennis;
 
 public class TennisGame6 : ITennisGame
@@ -25,54 +27,62 @@ public class TennisGame6 : ITennisGame
 
         if (player1.Score == player2.Score)
         {
-            // tie score
-            string tieScore = player1.Score switch
-            {
-                0 => "Love-All",
-                1 => "Fifteen-All",
-                2 => "Thirty-All",
-                _ => "Deuce",
-            };
+            string tieScore = GetTieScoreTerm();
             result = tieScore;
         }
         else if (player1.Score >= 4 || player2.Score >= 4)
         {
-            // end-game score
-            string endGameScore = (player1.Score - player2.Score) switch
-            {
-                1 => $"Advantage {player1.Name}",
-                -1 => $"Advantage {player2.Name}",
-                >= 2 => $"Win for {player1.Name}",
-                _ => $"Win for {player2.Name}",
-            };
+            string endGameScore = GetEndgameScoreTerm();
             result = endGameScore;
         }
         else
         {
             // regular score
             string regularScore;
-
-            var score1 = player1.Score switch
-            {
-                0 => "Love",
-                1 => "Fifteen",
-                2 => "Thirty",
-                _ => "Forty"
-            };
-
-            var score2 = player2.Score switch
-            {
-                0 => "Love",
-                1 => "Fifteen",
-                2 => "Thirty",
-                _ => "Forty"
-            };
-
+            string score1 = GetRegularScoreTerm(player1.Score);
+            string score2 = GetRegularScoreTerm(player2.Score);
+            
             regularScore = $"{score1}-{score2}";
 
             result = regularScore;
         }
 
         return result;
+    }
+
+    private static string GetRegularScoreTerm(int score)
+    {
+        return score switch
+        {
+            0 => "Love",
+            1 => "Fifteen",
+            2 => "Thirty",
+            _ => "Forty"
+        };
+    }
+
+    private string GetEndgameScoreTerm()
+    {
+        // end-game score
+        return (player1.Score - player2.Score) switch
+        {
+            1 => $"Advantage {player1.Name}",
+            -1 => $"Advantage {player2.Name}",
+            >= 2 => $"Win for {player1.Name}",
+            <=-2 => $"Win for {player2.Name}",
+            _ => throw new UnreachableException("Bad tie logic")
+        };
+    }
+
+    private string GetTieScoreTerm()
+    {
+        // tie score
+        return player1.Score switch
+        {
+            0 => "Love-All",
+            1 => "Fifteen-All",
+            2 => "Thirty-All",
+            _ => "Deuce",
+        };
     }
 }
