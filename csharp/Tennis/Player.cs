840a6cad59ea47e49d7df4ae114bce478e76fb7d
Gustav Arneving <gustav.arneving@mindroad.se>
2024-03-03 21:23:35 +0100

    Move player to separate file

---
+++ csharp/Tennis/Player.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Tennis;
+
+struct Player{
+        public Player(string name)
+        {
+            this.Score = 0;
+            this.Name = name;
+        }
+        public uint Score { get; set; }
+        public string Name { get; set; }
+    };
