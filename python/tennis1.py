bd843186fc89d2a6f8d6515deccf035807a3b987
Jonatan Skogsfors <jonatan.skogsfors@smhi.se>
2024-03-11 10:46:45 +0100

    Kommentarer i pattern matching
    Dokumentera både explicita och implicita antaganden.

--- python/tennis1.py
+++ python/tennis1.py
@@ -28,17 +28,24 @@ class TennisGame1:
         result = ""
         match (self.p1_points, self.p2_points):
             case [p1, p2] if p1 == p2 and p1 < Points.FORTY:
+                # Even points, below forty
                 result = f"{Points(p1).pretty()}-All"
             case [p1, p2] if p1 == p2:
+                # Even points, forty or above
                 result = "Deuce"
             case [p1, p2] if p1 <= Points.FORTY and p2 <= Points.FORTY:
+                # Different points, both below forty
                 result = f"{Points(p1).pretty()}-{Points(p2).pretty()}"
             case [p1, p2] if p1 - p2 == 1:
+                # One ball difference, above forty, player 1 advantage
                 result = f"Advantage {self.player1_name}"
             case [p1, p2] if p2 - p1 == 1:
+                # One ball difference, above forty, player 2 advantage
                 result = f"Advantage {self.player2_name}"
             case [p1, p2] if p1 - p2 > 1:
+                # More than one ball difference, player 1 above forty
                 result = f"Win for {self.player1_name}"
             case [p1, p2] if p2 - p1 > 1:
+                # More than one ball difference, player 2 above forty
                 result = f"Win for {self.player2_name}"
         return result
